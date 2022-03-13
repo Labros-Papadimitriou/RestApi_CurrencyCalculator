e6e6b39976dc50edb290619f8124d8a8859aa3dd
Labros Papadimitriou <papadimitriou.lb@gmail.com>
2022-03-13 14:51:42 +0200

    Removed Seed from appDbContesxt

--- RestApi_CurrencyCalculator/Persistence/ApplicationDbContext.cs
+++ RestApi_CurrencyCalculator/Persistence/ApplicationDbContext.cs
@@ -17,7 +17,6 @@ namespace RestApi_CurrencyCalculator.Persistence
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.RelateModels();
-            modelBuilder.Seed();
 
             base.OnModelCreating(modelBuilder);
         }
